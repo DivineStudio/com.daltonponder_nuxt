140df507af563782f4f694203cda843718af1b1c
Dalton Ponder <ponder.dalton.ph@gmail.com>
2025-10-19 01:54:22 -0500

    Adding the new logo to the header and updating styles to allow the new logo to properly contrast

--- app.vue
+++ app.vue
@@ -2,10 +2,14 @@
   <div>
     <header>
 
-      <nav class="navbar fixed bg-primary text-primary-content shadow-sm z-9999">
+      <nav class="navbar fixed bg-secondary text-secondary-content border-b-1 border-b-(--color-primary) shadow-sm z-9999">
         <div class="container px-8 mx-auto flex">
           <div class="navbar-start">
-            <NuxtLink to="/" class="btn btn-ghost text-xl">Dalton Ponder</NuxtLink>
+            <NuxtLink to="/" class="btn btn-ghost text-xl">
+              <img class="max-h-12"
+                   src="/logos/DP_FullLogo_250x84.webp"
+                   :alt="$t('Image.CodeRainAlt')" />
+            </NuxtLink>
           </div>
           <div class="navbar-center hidden lg:flex">
           </div>
